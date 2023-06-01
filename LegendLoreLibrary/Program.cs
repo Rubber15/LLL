09f57325660304e915f1dc85a5a2e7b309bdf44a
Rubber15 <113981419+Rubber15@users.noreply.github.com>
2023-06-01 10:05:04 +0200

    Full Update

--- LegendLoreLibrary/Program.cs
+++ LegendLoreLibrary/Program.cs
@@ -1,9 +1,8 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
 
 namespace LegendLoreLibrary
 {
